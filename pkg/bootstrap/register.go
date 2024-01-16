89b991b2c699ab553f589d0c090fcf4b8b01aef9
vuong <yeungon@gmail.com>
2024-01-16 09:18:00 +0700

    Refractored - register

---
+++ pkg/bootstrap/register.go
@@ -0,0 +1,15 @@
+package bootstrap
+
+import (
+	"github.com/kiwipanel/scaffolding/pkg/routing"
+	"github.com/kiwipanel/scaffolding/pkg/ui/static"
+	"github.com/kiwipanel/scaffolding/pkg/ui/view"
+)
+
+func Register() {
+	r := routing.GetRoute()
+	static.Register(r)
+	r.Renderer = view.RenderTemplates
+	routing.Register()
+	r.Logger.Fatal(r.Start(":7879"))
+}
