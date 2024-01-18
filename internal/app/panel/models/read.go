2f1203598add6e0b03ad4db514a79aec43e32896
vuong <yeungon@gmail.com>
2024-01-18 17:06:01 +0700

    Added the guard for the login page

--- internal/app/panel/models/read.go
+++ internal/app/panel/models/read.go
@@ -1,33 +1,25 @@
-package bootstrap
+package models
 
 import (
 	"fmt"
 
 	"github.com/kiwipanel/scaffolding/pkg/database"
 	"github.com/kiwipanel/scaffolding/pkg/database/schema"
-	"github.com/kiwipanel/scaffolding/pkg/helpers"
 )
 
-func FirstSetup() {
-	passcode := helpers.GenerateRandomString(9)
-	database.DB.Create(&schema.Panel{Version: "0.0.1", Passcode: passcode, Firsttime: true, Lock: false})
-}
-
-func Setup() {
+func ReadPanel() (schema.Panel, error) {
 	var panel schema.Panel
 	result := database.DB.First(&panel, 1)
-
 	if result.Error != nil {
-		// Handle the error (record not found, connection issue, etc.)
 		fmt.Println("Error:", result.Error)
 	} else if result.RowsAffected == 0 {
-		// Handle the case where no record was found
-		FirstSetup()
 		fmt.Println("No record found")
+		return panel, nil
 	} else {
 		// Record found, do something with 'panel'
 		fmt.Println("ID:", panel.ID)
 		fmt.Println(panel.Passcode)
+		return panel, nil
 	}
-
+	return panel, nil
 }
